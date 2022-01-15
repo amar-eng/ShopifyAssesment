a503cd12f454b4082f8c31225f82d4706e46a499
amar-eng <am24mo@gmail.com>
2022-01-15 06:00:44 -0500

    retreived and manipulated data from nasa apod archives

---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nasaphoto from '../components/Nasaphoto'
+import Navbar from '../components/Navbar'
+const Home = () => {
+
+    return (
+        <div>
+            <Navbar />
+            <Nasaphoto/>
+        </div>
+    )
+}
+
+export default Home
